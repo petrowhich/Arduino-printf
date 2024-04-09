ec4919cdb5481765a84e00971ddb1608806b44f1
Petrovich <132599168+petrowhich@users.noreply.github.com>
2024-04-09 18:23:44 +0300

    clang-format led.ino

--- examples/led.ino
+++ examples/led.ino
@@ -1,33 +1,28 @@
 #include "printf.h"
 
-enum ledIDNames {
-    LED_BUILTIN_ID = 0
-};
+enum ledIDNames { LED_BUILTIN_ID = 0 };
 
-const unsigned ledIDs[] = {
-    LED_BUILTIN
-};
+const unsigned ledIDs[] = {LED_BUILTIN};
 
-byte ledStates[] = {
-    LOW
-};
+byte ledStates[] = {LOW};
 
 void switchLed(unsigned id) {
-    digitalWrite(ledIDs[id], ledStates[id] = !ledStates[id]);
+  digitalWrite(ledIDs[id], ledStates[id] = !ledStates[id]);
 }
 
 void debugInformation(unsigned id) {
-    printf("LED on pin %d is %s\n", ledIDs[id], ledStates[id] ? "on" : "off");
+  printf("LED on pin %d is %s\n", ledIDs[id], ledStates[id] ? "on" : "off");
 }
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(9600);
-    while (!Serial) {}
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+  }
 }
 
 void loop() {
-    switchLed(LED_BUILTIN_ID);
-    debugInformation(LED_BUILTIN_ID);
-    delay(1000);
+  switchLed(LED_BUILTIN_ID);
+  debugInformation(LED_BUILTIN_ID);
+  delay(1000);
 }
