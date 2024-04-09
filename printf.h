43cf87037cc3435b315b2ec22cb594d852cfcd94
Petrovich <132599168+petrowhich@users.noreply.github.com>
2024-04-09 18:22:00 +0300

    Add printf.cpp and printf.h

---
+++ printf.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* -*- linux-c -*- ------------------------------------------------------- *
+ *
+ *   Copyright (C) 1991, 1992 Linus Torvalds
+ *   Copyright 2007 rPath, Inc. - All Rights Reserved
+ *   Copyright 2009 Intel Corporation; author H. Peter Anvin
+ *
+ * ----------------------------------------------------------------------- */
+
+#pragma once
+
+#include <Arduino.h>
+#include <stdarg.h>
+
+/* printf.c */
+int sprintf(char *buf, const char *fmt, ...);
+int vsprintf(char *buf, const char *fmt, va_list args);
+int printf(const char *fmt, ...);
